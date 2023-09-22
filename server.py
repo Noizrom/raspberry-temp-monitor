cbd8537ba1c5c61747b77e96bb61dc3509d4e83f
Noizrom <noizrom@gmail.com>
2023-09-22 14:04:20 +0800

    a much more effient handler for new data

--- server.py
+++ server.py
@@ -1,23 +1,50 @@
 """Server for Raspberry Pi temperature monitoring app"""
+# pylint: disable=broad-except
 
 import os
+import random  # debug
 import asyncio
 from subprocess import getoutput
+from enum import StrEnum
 
 from fastapi import FastAPI, WebSocket
 from fastapi.staticfiles import StaticFiles
 from websockets.exceptions import ConnectionClosedOK
 import paramiko
 
-import random  # debug
 
 app = FastAPI()
 
 
+DEV = False
 INTERVAL = 1.5
 
 
+class Devices(StrEnum):
+    """devices"""
+
+    RASPBERRY1 = "raspberry1"
+    RASPBERRY2 = "raspberry2"
+
+
+def wrap_for_dev(func):
+    """wrap function for development"""
+    if DEV:
+        print("DEV MODE")
+
+        async def wrapper():
+            while True:
+                yield random.randint(0, 100)
+                await asyncio.sleep(INTERVAL)
+
+        return wrapper
+    else:
+        return func
+
+
+@wrap_for_dev
 async def raspberry2_generator() -> float:
+    """a generator for raspberry2 temperature"""
     # Initialize an SSH client
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -48,15 +75,13 @@ async def raspberry2_generator() -> float:
             # Wait for one second before the next iteration
             await asyncio.sleep(INTERVAL)
 
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
+    except Exception as error:
+        print(f"An error occurred: {str(error)}")
 
 
+@wrap_for_dev
 async def raspberry1_generator() -> float:
     """get temperature from device"""
-    # while True:
-    #     yield random.random() * 100
-    #     await asyncio.sleep(1)
     while True:
         raw_text = getoutput("vcgencmd measure_temp")
         float_text = raw_text.removeprefix("temp=").removesuffix("'C")
@@ -64,32 +89,98 @@ async def raspberry1_generator() -> float:
         await asyncio.sleep(INTERVAL)
 
 
+class Broadcaster:
+    """Manages websocket connections"""
+
+    def __init__(self):
+        self.subscribers: dict[str, list] = {}
+
+    async def subscribe(self, device: Devices, websocket: WebSocket):
+        """subscribe to a device"""
+        await websocket.accept()
+        subpar = self.subscribers.get(device, [])
+        subpar.append(websocket)
+        self.subscribers[device] = subpar
+
+    async def update_rasp1_subscribers(self):
+        """update subscribers of raspberry1"""
+        async for new_data in raspberry1_generator():
+            subscribers = self.subscribers.get(Devices.RASPBERRY1, [])
+
+            # will loop here until there is a subscriber
+            while not subscribers:
+                # print("No Subscriber. not asking for new data")
+                await asyncio.sleep(2)
+                subscribers = self.subscribers.get(Devices.RASPBERRY1, [])
+
+            for subscriber in self.subscribers.get(Devices.RASPBERRY1, []):
+                try:
+                    await subscriber.send_json({"data": new_data})
+                except ConnectionClosedOK:
+                    print("A subscriber is disconnected. removing.. .")
+                    for sub in self.subscribers[Devices.RASPBERRY1]:
+                        if sub == subscriber:
+                            self.subscribers[Devices.RASPBERRY1].remove(sub)
+                            print("Removed : ", sub)
+                            break
+                except Exception as error:
+                    print("Unhandled Exemption : ", error)
+                    continue
+
+    async def update_rasp2_subscribers(self):
+        """update subscribers of raspberry2"""
+        async for new_data in raspberry2_generator():
+            subscribers = self.subscribers.get(Devices.RASPBERRY2, [])
+            while not subscribers:
+                # print("No Subscriber. not asking for new data")
+                await asyncio.sleep(2)
+                subscribers = self.subscribers.get(Devices.RASPBERRY2, [])
+
+            for subscriber in self.subscribers.get(Devices.RASPBERRY2, []):
+                try:
+                    await subscriber.send_json({"data": new_data})
+                except ConnectionClosedOK:
+                    print("A subscriber is disconnected. removing.. .")
+                    for sub in self.subscribers[Devices.RASPBERRY2]:
+                        if sub == subscriber:
+                            self.subscribers[Devices.RASPBERRY2].remove(sub)
+                            print("Removed : ", sub)
+                            break
+                except Exception as error:
+                    print("Unhandled Exemption : ", error)
+                    continue
+
+    async def begin_if_someone_subscribed(self):
+        """begin updating if someone subscribed"""
+        print("=begin_if_someone_subscribed=")
+        ras1_task = asyncio.create_task(self.update_rasp1_subscribers())
+        ras2_task = asyncio.create_task(self.update_rasp2_subscribers())
+        await asyncio.gather(ras1_task, ras2_task)
+
+
+broadcaster = Broadcaster()
+
+
+@app.on_event("startup")
+async def startup_event():
+    """start background tasks"""
+    asyncio.create_task(broadcaster.begin_if_someone_subscribed())
+
+
 @app.websocket("/raspberry2")
 async def liveupdate2(websocket: WebSocket):
     """sends live update of device temperature"""
-    await websocket.accept()
-    try:
-        async for data in raspberry2_generator():
-            await websocket.send_json({"data": data})
-    except ConnectionClosedOK:
-        pass
+    await broadcaster.subscribe(Devices.RASPBERRY2, websocket)
+    while websocket.client_state != 3:  # keeps the connection alive
+        await asyncio.sleep(10)
 
 
 @app.websocket("/raspberry1")
 async def liveupdate(websocket: WebSocket):
     """sends live update of device temperature"""
-    await websocket.accept()
-    try:
-        async for data in raspberry1_generator():
-            await websocket.send_json({"data": data})
-    except ConnectionClosedOK:
-        pass
-    # while websocket.client_state != 3:  # 3 = closed
-    #     try:
-    #         await asyncio.sleep(1)
-    #         await websocket.send_json({"data": await get_temp_rasp1()})
-    #     except ConnectionClosedOK:
-    #         break
+    await broadcaster.subscribe(Devices.RASPBERRY1, websocket)
+    while websocket.client_state != 3:  # keeps the connection alive
+        await asyncio.sleep(10)
 
 
 # mount dist folder
